1c8df6b444b22fa809628bdc196d7a38b883a9c1
bruno <devnull@localhost>
2010-03-16 18:43:33 +0000

    Primeira iteracao da GUI da nova_instalacao. ticket #204

--- nova_instalacao/index.php
+++ nova_instalacao/index.php
@@ -2,88 +2,159 @@
 
 include 'instalacao.inc';
 
-/*
-1a Etapa:
+/* Para simplificar, a instalacao possui um template, para
+ * o qual mandaremos o resultado da etapa atual, o controle de
+ * etapas sera feito atraves de uma variavel e posts para a mesma
+ * pagina. */
 
-Pré-Req: php-mysql
-Executar: Informar Nome do Banco, Usuário, Senha e Host tentar criar o banco se não existir e importar o conteúdo para o BD.
-Criar o teleduc.conf com as informações acima. Se não for possível pedir ao usuário e dar o conteúdo. */
+session_start("instalacao_teleduc4");
+
+$content = "";
 if (!isset($_POST['etapa'])){
-	$etapa = 1;
+	$etapa = 0;
+} else {
+	$etapa = $_POST['etapa'];
 }
 
 
-if (!VerificaRegisterGlobals()){
-	die("É necessário habilitar o register_globals.");
-}
+if ($etapa == 0){
 
-if (!VerificaPHPMysql()){
-	die("Não foi encontrado o modulo php-mysql.");
-}
+	/* Erro na Instalacao */
+	if (!VerificaRegisterGlobals()){
 
-/* USER INPUT */
-$dbname = "TelEducDiego";
-$dbnamecurso = "TelEducDiego";
-$dbuser = "root";
-$dbpwd = "Myhel80n";
-$dbhost = "localhost";
-$dbport = "3306";
-
-if (!$sock = VerificaExistenciaBD($dbname, $dbuser, $dbpwd, $dbhost, $dbport)){
-	if (!CriaBasePrincipal($dbname, $dbuser, $dbpwd, $dbhost, $dbport)){
-		die("Não foi possível criar o Banco de Dados: ".mysql_error());
-	} else {
-		$sock = mysql_connect($dbhost.":".$dbport, $dbuser, $dbpwd);
-		mysql_select_db($dbname, $sock);
-				
-	}
-} 
-
-InicializaBD($sock);
-
-if (!VerificaExistenciaArq("../cursos/aplic/bibliotecas/teleduc.inc")){
-	$conteudo = CriaTelEducInc($dbname, $dbnamecurso, $dbuser, $dbpwd, $dbhost, $dbport);
-	if ($conteudo !== true){
-		echo("Não foi possível criar o arquivo teleduc.inc, crie manualmente.<br /> Com o conteudo: <br /><br />");
-		echo("<textarea cols='70' rows='15'>".str_replace(";",";\n",$conteudo)."</textarea>");
+		$content_header = "Erro: Diretiva register_globals desativada.";
+		$content .= "<p>É necessário habilitar o register_globals.</p>";
+		$content .= "<p>Edite seu /etc/php.ini ou então tutorial dreamhost</p>";
+		include 'template_instalacao.php';
 		exit();
+
+	} else {
+		$content .= "<p>A diretiva register_globals está habilitado conforme necessário... OK</p>";
 	}
+
+	/* Erro na Instalacao */
+	if (!VerificaPHPMysql()){
+
+		$content_header = "Erro: Modulo php-mysql não encontrado.";
+		$content .= "<p>É necessário instalar o módulo php-mysql.</p>";
+		$content .= "<p>yum install php-mysql genericos.</p>";
+		include 'template_instalacao.php';
+		exit();
+
+	} else {
+		$content .= "<p>O modulo php-mysql está instalado conforme necessário... OK</p>";
+	}
+	
+	$content_header = "Etapa 0 - Banco de Dados e Arquivo de Configuração:";
+
+	$content .= "<form method='POST' action='index.php'>";
+	$content .= "<input type=text name=dbname value='TelEduc4'/><br />";
+	$content .= "<input type=text name=dbnamecurso value='TelEduc4Curso'/><br />";
+	$content .= "<input type=text name=dbuser value='usuario'/><br />";
+	$content .= "<input type=password name=dbpwd value='senha'/><br />";
+	$content .= "<input type=text name=dbhost value='localhost'/><br />";
+	$content .= "<input type=text name=dbport value='3306'/><br />";
+	$content .= "<input type=hidden name=etapa value='1'/><br />";
+	$content .= "<input type=submit value='Enviar'/><br />";
+	$content .= "</form>";
+
+} else if ($etapa == 1){
+	
+	/* Salva na sessao a informacao do banco de dados */
+	$_SESSION['dbname'] = $dbname;
+	$_SESSION['dbuser']  = $dbuser;
+	$_SESSION['dbpwd'] = $dbpwd;
+	$_SESSION['dbhost'] = $dbhost; 
+	$_SESSION['dbport'] = $dbport;
+
+	if (!$sock = VerificaExistenciaBD($dbname, $dbuser, $dbpwd, $dbhost, $dbport)){
+
+		/* Erro na Instalacao */
+		if (!CriaBasePrincipal($dbname, $dbuser, $dbpwd, $dbhost, $dbport)){
+			$content_header = "Erro: Não foi possível criar o banco de dados principal";
+			$content .= "Não foi possível criar o Banco de Dados: ".mysql_error();
+			$content .= "Verifique os dados, crie na mão";
+			include 'template_instalacao.php';
+			exit();
+		} else {
+			$sock = mysql_connect($dbhost.":".$dbport, $dbuser, $dbpwd);
+			mysql_select_db($dbname, $sock);
+
+		}
+	}
+
+	InicializaBD($sock);
+
+	if (!VerificaExistenciaArq("../cursos/aplic/bibliotecas/teleduc.inc")){
+		$conteudo = CriaTelEducInc($dbname, $dbnamecurso, $dbuser, $dbpwd, $dbhost, $dbport);
+
+		/* Erro na Instalacao */
+		if ($conteudo !== true){
+			$content_header = "Erro: Não foi possível criar o teleduc.inc";
+			$content .= "Não foi possível criar o arquivo teleduc.inc, crie manualmente.<br /> Com o conteudo: <br /><br />";
+			$content .= "<textarea cols='70' rows='15'>".str_replace(";",";\n",$conteudo)."</textarea>";
+			include 'template_instalacao.php';
+			exit();
+		}
+	}
+
+	$content_header = "Etapa 1 - Host e Diretórios:";
+
+	$content .= "<form method='POST' action='index.php'>";
+	$content .= "<input type=text name=host value='www.dominio.com.br'/><br />";
+	$content .= "<input type=text name=www value='/ead/teleduc4'/><br />";
+	$content .= "<input type=text name=arquivos value='/home/arquivos'/><br />";
+	$content .= "<input type=text name=sendmail value='/usr/sbin/sendmail'/><br />";
+	$content .= "<input type=hidden name=etapa value='2'/><br />";
+	$content .= "<input type=submit value='Enviar'/><br />";
+	$content .= "</form>";
+
+} else if ($etapa == 2){
+
+	$content_header = "Etapa 2 - Configurações do Administrador do Ambiente:";
+
+	/*
+	 2a Etapa:
+	 Pré-Req: 1a Etapa
+	 Executar: Escolher pasta para arquivos (?), adivinhar host e caminho pela url.
+	 Configurar os demais diretorios, (rever necessidade de alguns deles). */
+
+	$sock = mysql_connect($dbhost.":".$dbport, $dbuser, $dbpwd);
+	mysql_select_db($dbname, $sock);
+
+	RegistraConfiguracoes($sock, $host, $www, $arquivos, $sendmail);
+
+
+	$content .= "<form method='POST' action='index.php'>";
+	$content .= "<input type=text name=admtele_nome value='Nome Sobrenome'/><br />";
+	$content .= "<input type=text name=admtele_email value='nome@email.com'/><br />";
+	$content .= "<input type=password name=admtele_senha value='AA2.FEIabj1C6'/><br />";
+	$content .= "<input type=hidden name=etapa value='3'/><br />";
+	$content .= "<input type=submit value='Enviar'/><br />";
+	$content .= "</form>";
+
+
+
+} else if ($etapa == 3){
+
+	/*
+	 3a Etapa:
+	 Pré-Req: 2a Etapa
+	 Executar: Pedir ao admin colocar as tarefas do cron, perguntar o email do admtele e a senha para admtele. */
+	
+	$sock = mysql_connect($dbhost.":".$dbport, $dbuser, $dbpwd);
+	mysql_select_db($dbname, $sock);
+
+	RegistraDadosAdmtele($sock, $admtele_nome, $admtele_email, $admtele_senha);
+
+	$content_header = "Etapa 3 - Fim ~!:";
+	$content = "OK, cron";
+
 }
 
+include 'template_instalacao.php';
 
-/*
-2a Etapa:
-Pré-Req: 1a Etapa
-Executar: Escolher pasta para arquivos (?), adivinhar host e caminho pela url.
-Configurar os demais diretorios, (rever necessidade de alguns deles). */
-
-/* USER INPUT - Pré-Preenchidas */
-$host = "localhost";
-$www = "/~diego/teleduc4";
-$arquivos = "/home/diego/public_html/teleduc4/arquivos";
-$sendmail = "/usr/sbin/sendmail";
-
-RegistraConfiguracoes($sock, $host, $www, $arquivos, $sendmail);
-
-/*
-3a Etapa:
-Pré-Req: 2a Etapa
-Executar: Pedir ao admin colocar as tarefas do cron, perguntar o email do admtele e a senha para admtele. */
-
-/* USER INPUT */
-$admtele_nome = "Bruno Buccolo";
-$admtele_email = "admtele@gmail.com";
-$admtele_senha = "AA2.FEIabj1C6";
-
-RegistraDadosAdmtele($sock, $admtele_nome, $admtele_email, $admtele_senha);
-
-echo("Não esqueça de configurar o cron.");
-
-/*
-4a Etapa:
-Pré-Req: 3a Etapa
-Executar: Fim? Feedback e botão de entrar. */
-
+exit();
 
 
 ?>
---
+++ nova_instalacao/template_instalacao.php
@@ -0,0 +1,30 @@
+<html>
+<head>
+	<title>TelEduc 4 - Instalacao do Ambiente</title>
+	<link rel="stylesheet" type="text/css" href="instalacao.css">
+</head>
+
+<body>
+	<div class="container">
+		<div id="logo"><img src="../imgs/logo.gif"/></div>
+		<div id="header"><h1>Instalação do TelEduc 4</h1></div>
+		<div class="content-header">
+			<p><?php echo $content_header;?></p>
+		</div>
+		<div class="content">
+			<p><?php echo $content;?></p>
+		</div>
+		<div class="footer">
+			<div id="logo_sampler">
+				<a href="http://www.nied.unicamp.br"><img src="../imgs/logoNied.gif" alt="nied" border="0" style="margin-right:8px;" /></a>
+			  	<a href="http://www.ic.unicamp.br"><img src="../imgs/logoInstComp.gif" alt="Instituto de Computa&ccedil;&atilde;o" border="0" style="margin-right:6px;" /></a>
+			  	<a href="http://www.unicamp.br"><img src="../imgs/logoUnicamp.gif" alt="UNICAMP" border="0" /></a>
+			 </div>
+			 <div id="signature">
+			  	<p>2008  - TelEduc - Todos os direitos reservados. All rights reserved - NIED - UNICAMP</p>
+			</div>
+		</div>
+	</div>
+</body>
+
+</html>
