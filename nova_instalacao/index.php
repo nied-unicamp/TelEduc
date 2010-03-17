95a6b008d11d9ed9c46ab97c775a23323f2d0694
bruno <devnull@localhost>
2010-03-17 16:14:30 +0000

    Pequenas correcoes de CSS e melhorias na interface.

--- nova_instalacao/index.php
+++ nova_instalacao/index.php
@@ -22,69 +22,77 @@ if ($etapa == 0){
 	/* Erro na Instalacao */
 	if (!VerificaRegisterGlobals()){
 
-		$content_header = "Erro: Diretiva register_globals desativada.";
-		$content .= "<p>É necessário habilitar o register_globals.</p>";
+		$content_header = "Erro na Instalação: A diretiva register_globals está desativada.";
+		$content .= "<p>É necessário habilitar a diretiva register_globals. <img src='../cursos/aplic/imgs/errado.png'></p>";
 		$content .= "<p>Edite seu /etc/php.ini ou então tutorial dreamhost</p>";
+		$content .= "<input type='button' value='Voltar' onClick='history.go(-1)'>";
+		$content .= "<input type='button' value='Tentar Novamente' onClick='history.go(0)'>";
 		include 'template_instalacao.php';
 		exit();
 
 	} else {
 		
-		$content .= "<p>A diretiva register_globals está habilitado conforme necessário... OK</p>";
+		$content .= "<p class=feedbackp>A diretiva register_globals está habilitada. <img src='../cursos/aplic/imgs/certo.png' alt='com sucesso'></p>";
 		
 	}
 
 	/* Erro na Instalacao */
 	if (!VerificaPHPMysql()){
 
-		$content_header = "Erro: Modulo php-mysql não encontrado.";
-		$content .= "<p>É necessário instalar o módulo php-mysql.</p>";
+		$content_header = "Erro na Instalação: Módulo php-mysql não encontrado.";
+		$content .= "<p>É necessário instalar o módulo php-mysql. <img src='../cursos/aplic/imgs/errado.png'></p>";
 		$content .= "<p>yum install php-mysql genericos.</p>";
+		$content .= "<input type='button' value='Voltar' onClick='history.go(-1)'>";
+		$content .= "<input type='button' value='Tentar Novamente' onClick='history.go(0)'>";
 		include 'template_instalacao.php';
 		exit();
 
 	} else {
 		
-		$content .= "<p>O modulo php-mysql está instalado conforme necessário... OK</p>";
+		$content .= "<p class=feedbackp>O modulo php-mysql está instalado. <img src='../cursos/aplic/imgs/certo.png' alt='com sucesso'></p>";
 	
 	}
 	
-	$content_header = "Etapa 0 - Banco de Dados e Arquivo de Configuração:";
+	$content_header = "Etapa 1 - Banco de Dados e Arquivo de Configuração";
 
 	$content .= "<br /><br />";
 	$content .= "<form method='POST' action='index.php'>";
-	$content .= "<label class=form for=dbname>Nome do Banco de Dados Principal</label>";
-	$content .= "<input class=form type=text name=dbname value='TelEduc4'/><br />";
-	$content .= "<label class=form for=dbnamecurso>Prefixo dos Bancos de Dados dos Cursos</label>";
-	$content .= "<input class=form type=text name=dbnamecurso value='TelEduc4Curso'/><br />";
-	$content .= "<label class=form for=dbuser>Usuario do MySQL</label>";
-	$content .= "<input class=form type=text name=dbuser value='usuario'/><br />";
-	$content .= "<label class=form for=dbpwd>Senha do MySQL</label>";
-	$content .= "<input class=form type=password name=dbpwd value='senha'/><br />";
-	$content .= "<label class=form for=dbhost>Host do MySQL (link)</label>";
-	$content .= "<input class=form type=text name=dbhost value='localhost'/><br />";
-	$content .= "<label class=form for=dbport>Porta do MySQL</label>";
-	$content .= "<input class=form type=text name=dbport value='3306'/><br />";
-	$content .= "<input class=form type=hidden name=etapa value='1'/><br />";
-	$content .= "<input type=submit value='Enviar'/><br />";
+	$content .= "<label class='form' for=dbname>Nome do Banco de Dados Principal</label>";
+	$content .= "<input class='form' size=25 type=text name=dbname value='".(isset($_SESSION['dbname']) ? $_SESSION['dbname'] : 'TelEduc4')."'/><br />";
+	$content .= "<label class='form' for=dbnamecurso>Prefixo dos Bancos de Dados dos Cursos</label>";
+	$content .= "<input class='form' size=25 type=text name=dbnamecurso value='".(isset($_SESSION['dbnamecurso']) ? $_SESSION['dbnamecurso'] : 'TelEduc4Curso_')."'/><br />";
+	$content .= "<label class='form' for=dbuser>Usuario do MySQL</label>";
+	$content .= "<input class='form' size=25 type=text name=dbuser value='".(isset($_SESSION['dbuser']) ? $_SESSION['dbuser'] : 'usuario')."'/><br />";
+	$content .= "<label class='form' for=dbpwd>Senha do MySQL</label>";
+	$content .= "<input class='form' size=25 type=password name=dbpwd value='".(isset($_SESSION['dbpwd']) ? $_SESSION['dbpwd'] : 'senha')."'/><br />";
+	$content .= "<label class='form' for=dbhost>Host do MySQL (link)</label>";
+	$content .= "<input class='form' size=25 type=text name=dbhost value='".(isset($_SESSION['dbhost']) ? $_SESSION['dbhost'] : 'localhost')."'/><br />";
+	$content .= "<label class='form' for=dbport>Porta do MySQL</label>";
+	$content .= "<input class='form' size=25 type=text name=dbport value='".(isset($_SESSION['dbport']) ? $_SESSION['dbport'] : '3306')."'/><br />";
+	$content .= "<input class='form' type=hidden name=etapa value='1'/><br />";
+	$content .= "<input type=submit value='Prosseguir' class='form'/><br />";
 	$content .= "</form>";
 
 } else if ($etapa == 1){
 	
 	/* Salva na sessao a informacao do banco de dados */
-	$_SESSION['dbname'] = $dbname;
-	$_SESSION['dbuser']  = $dbuser;
-	$_SESSION['dbpwd'] = $dbpwd;
-	$_SESSION['dbhost'] = $dbhost; 
-	$_SESSION['dbport'] = $dbport;
+	$_SESSION['dbname'] = $_POST['dbname'];
+	$_SESSION['dbnamecurso'] = $_POST['dbnamecurso'];
+	$_SESSION['dbuser']  = $_POST['dbuser'];
+	$_SESSION['dbpwd'] = $_POST['dbpwd'];
+	$_SESSION['dbhost'] = $_POST['dbhost'];
+	$_SESSION['dbport'] = $_POST['dbport'];
 
 	if (!$sock = VerificaExistenciaBD($dbname, $dbuser, $dbpwd, $dbhost, $dbport)){
 
 		/* Erro na Instalacao */
 		if (!CriaBasePrincipal($dbname, $dbuser, $dbpwd, $dbhost, $dbport)){
-			$content_header = "Erro: Não foi possível criar o banco de dados principal";
-			$content .= "Não foi possível criar o Banco de Dados: ".mysql_error();
-			$content .= "Verifique os dados, crie na mão";
+			$content_header = "Erro: Não foi possível criar o banco de dados principal.";
+			$content .= "<p>Não foi possível criar o banco de dados. <img src='../cursos/aplic/imgs/errado.png'><p/>";
+			$content .= "<p>".mysql_error()."</p>";
+			$content .= "<p>Verifique os dados, crie na mão ou de permissao</p>";
+			$content .= "<input type='button' value='Voltar' onClick='history.go(-1)'>";
+			$content .= "<input type='button' value='Tentar Novamente' onClick='history.go(0)'>";
 			include 'template_instalacao.php';
 			exit();
 		} else {
@@ -94,11 +102,11 @@ if ($etapa == 0){
 		}
 	}
 	
-	$content .= "<p>O banco de dados foi criado com sucesso... OK</p>";
+	$content .= "<p class=feedbackp>O banco de dados foi criado. <img src='../cursos/aplic/imgs/certo.png' alt='com sucesso'></p>";
 
 	InicializaBD($sock);
 	
-	$content .= "<p>O banco de dados foi inicializado com sucesso... OK</p>";
+	$content .= "<p class=feedbackp>O banco de dados foi inicializado. <img src='../cursos/aplic/imgs/certo.png' alt='com sucesso'></p>";
 	
 
 	if (!VerificaExistenciaArq("../cursos/aplic/bibliotecas/teleduc.inc")){
@@ -107,8 +115,11 @@ if ($etapa == 0){
 		/* Erro na Instalacao */
 		if ($conteudo !== true){
 			$content_header = "Erro: Não foi possível criar o teleduc.inc";
-			$content .= "Não foi possível criar o arquivo teleduc.inc, crie manualmente.<br /> Com o conteudo: <br /><br />";
+			$content .= "<p>Não foi possível criar o arquivo teleduc.inc. <img src='../cursos/aplic/imgs/errado.png'></p>";
+			$content .= "<p>Devido a uma permissao blabla... pode criar na mão, please?</p>";
 			$content .= "<textarea cols='70' rows='15'>".str_replace(";",";\n",$conteudo)."</textarea>";
+			$content .= "<input type='button' value='Voltar' onClick='history.go(-1)'>";
+			$content .= "<input type='button' value='Tentar Novamente' onClick='history.go(0)'>";
 			include 'template_instalacao.php';
 			exit();
 		}
@@ -116,27 +127,27 @@ if ($etapa == 0){
 	
 	
 	
-	$content .= "<p>O arquivo de configuração teleduc.inc foi criado com sucesso... OK</p>";
+	$content .= "<p class=feedbackp>O arquivo de configuração teleduc.inc foi criado. <img src='../cursos/aplic/imgs/certo.png' alt='com sucesso'></p>";
 
-	$content_header = "Etapa 1 - Host e Diretórios:";
+	$content_header = "Etapa 2 - Host e Diretórios";
 
 	$content .= "<br /><br />";
 	$content .= "<form method='POST' action='index.php'>";
-	$content .= "<label class=form for=host>Host do TelEduc</label>";
-	$content .= "<input type=text name=host value='".$_SERVER['SERVER_NAME']."'/><br />";
-	$content .= "<label class=form for=www>Caminho do TelEduc</label>";
-	$content .= "<input type=text name=www value='".str_replace("nova_instalacao/index.php", "", $_SERVER['PHP_SELF'])."'/><br />";
-	$content .= "<label class=form for=arquivos>Pasta dos Arquivos</label>";
-	$content .= "<input type=text name=arquivos value='/home/arquivos'/><br />";
-	$content .= "<label class=form for=sendmail>Caminho do Sendmail</label>";
-	$content .= "<input type=text name=sendmail value='/usr/sbin/sendmail'/><br />";
+	$content .= "<label class='form' for=host>Host do TelEduc</label>";
+	$content .= "<input type=text size=25 class='form' name=host value='".$_SERVER['SERVER_NAME']."'/><br />";
+	$content .= "<label class='form' for=www>Caminho do TelEduc</label>";
+	$content .= "<input type=text size=25 class='form' name=www value='".str_replace("nova_instalacao/index.php", "", $_SERVER['PHP_SELF'])."'/><br />";
+	$content .= "<label class='form' for=arquivos>Pasta dos Arquivos</label>";
+	$content .= "<input type=text size=25 class='form' name=arquivos value='/home/arquivos'/><br />";
+	$content .= "<label class='form' for=sendmail>Caminho do Sendmail</label>";
+	$content .= "<input type=text size=25 class='form' name=sendmail value='/usr/sbin/sendmail'/><br />";
 	$content .= "<input type=hidden name=etapa value='2'/><br />";
-	$content .= "<input type=submit value='Enviar'/><br />";
+	$content .= "<input type=submit value='Prosseguir' class='form'/><br />";
 	$content .= "</form>";
 
 } else if ($etapa == 2){
 
-	$content_header = "Etapa 2 - Configurações do Administrador do Ambiente:";
+	$content_header = "Etapa 3 - Administrador do Ambiente";
 
 	/*
 	 2a Etapa:
@@ -149,19 +160,19 @@ if ($etapa == 0){
 
 	RegistraConfiguracoes($sock, $host, $www, $arquivos, $sendmail);
 	
-	$content .= "<p>As configurações de diretorio foram salvas com sucesso... OK</p>";
+	$content .= "<p class=feedbackp>As configurações de diretorio foram salvas. <img src='../cursos/aplic/imgs/certo.png' alt='com sucesso'></p>";
 
 
 	$content .= "<br /><br />";
 	$content .= "<form method='POST' action='index.php'>";
-	$content .= "<label class=form for=admtele_nome>Nome do Administrador do Ambiente</label>";
-	$content .= "<input type=text name=admtele_nome value='Nome Sobrenome'/><br />";
-	$content .= "<label class=form for=admtele_email>E-Mail do Administrador do Ambiente</label>";
-	$content .= "<input type=text name=admtele_email value='nome@email.com'/><br />";
-	$content .= "<label class=form for=admtele_senha>Senha do Administrador do Ambiente</label>";
-	$content .= "<input type=password name=admtele_senha value='AA2.FEIabj1C6'/><br />";
+	$content .= "<label class='form' for=admtele_nome>Nome do Administrador do Ambiente</label>";
+	$content .= "<input type=text size=25 class='form' name=admtele_nome value='Nome Sobrenome'/><br />";
+	$content .= "<label class='form' for=admtele_email>E-Mail do Administrador do Ambiente</label>";
+	$content .= "<input type=text size=25 class='form' name=admtele_email value='nome@email.com'/><br />";
+	$content .= "<label class='form' for=admtele_senha>Senha do Administrador do Ambiente</label>";
+	$content .= "<input type=password size=25 class='form' name=admtele_senha value='AA2.FEIabj1C6'/><br />";
 	$content .= "<input type=hidden name=etapa value='3'/><br />";
-	$content .= "<input type=submit value='Enviar'/><br />";
+	$content .= "<input type=submit value='Prosseguir' class='form'/><br />";
 	$content .= "</form>";
 
 
@@ -178,10 +189,15 @@ if ($etapa == 0){
 
 	RegistraDadosAdmtele($sock, $admtele_nome, $admtele_email, $admtele_senha);
 	
-	$content .= "<p>As configurações do administrador do sistema (admtele) foram salvas com sucesso... OK</p>";
+	$content .= "<p class=feedbackp>As configurações do administrador do sistema (admtele) foram salvas. <img src='../cursos/aplic/imgs/certo.png' alt='com sucesso'></p>";
 
-	$content_header = "Etapa 3 - Fim ~!:";
-	$content = "OK terminou, só falta gravar as coisas no cron";
+	
+	$content_header = "Etapa 4 - Fim da Instalação";
+	
+	$content .= "<br /><br />";
+	$content .= "<p>OK terminou, só falta gravar as coisas no cron</p>";
+	$content .= "<p>Dicas apache e php</p>";
+	$content .= "<p>botao pra entrar no teleduc e indicacoes pra apagar o nova_instalacao</p>";
 
 }
 
