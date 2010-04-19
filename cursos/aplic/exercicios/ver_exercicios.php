7dff6e2ef05c66d890902395e82a1f3a8ece1588
richard <devnull@localhost>
2010-04-19 14:30:18 +0000

    Aparecia o link p/ recorrigir para não formadores. Apesar de que eles não tinham acesso a recorreção.

--- cursos/aplic/exercicios/ver_exercicios.php
+++ cursos/aplic/exercicios/ver_exercicios.php
@@ -339,7 +339,11 @@ if(count($exercicios) > 0 && $exercicios != null)
 		 */
 		
 		if($linha_item['corrigida'] == 'S'){
-			echo("                    <td align=\"left\">".$icone."<a href='ver_gabarito.php?cod_curso=".$cod_curso."&visualizar=I&cod=".$cod_usuario_exercicio."&cod_resolucao=".$cod_resolucao."'>".$linha_item['titulo']."</a> - <a href=\"corrigir_exercicio.php?cod_curso=".$cod_curso."&cod_resolucao=".$linha_item['cod_resolucao']."\"> (Recorrigir)</a> </td>\n");
+			echo("                    <td align=\"left\">".$icone."<a href='ver_gabarito.php?cod_curso=".$cod_curso."&visualizar=I&cod=".$cod_usuario_exercicio."&cod_resolucao=".$cod_resolucao."'>".$linha_item['titulo']."</a>");
+			if($eformador){
+				echo (" - <a href=\"corrigir_exercicio.php?cod_curso=".$cod_curso."&cod_resolucao=".$linha_item['cod_resolucao']."\"> (Recorrigir)</a> ");
+			}
+			echo("</td>\n");
 		} else  if($linha_item['submetida'] == 'S' && $eformador){
 			echo("                    <td align=\"left\">".$icone."<a href=\"corrigir_exercicio.php?cod_curso=".$cod_curso."&cod_resolucao=".$linha_item['cod_resolucao']."\">".$linha_item['titulo']."</a>");
 		 } else {
