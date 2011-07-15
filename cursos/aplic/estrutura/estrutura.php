39679f0cc53b1b644f4bc1078c8d5c33b0dc3b3d
Filipe Oshiro <filipe.oshiro@gmail.com>
2011-07-15 17:54:34 -0300

    Previa da descricao de Usuarios Online.

--- cursos/aplic/estrutura/estrutura.php
+++ cursos/aplic/estrutura/estrutura.php
@@ -154,7 +154,7 @@
   /* 10 - Ferramentas do ambiente */
   echo("                    <tr class=\"head01\" id=\"tr_ferrAmbiente\">\n");
   echo("                      <td align=left><b>".RetornaFraseDaLista($lista_frases,10)."</b></td>\n");
-  echo("                      <td width=5%><span id=\"span_ferrAmbiente\" class=\"link\" onClick=\"MostraTr(this.id,".$i.",18);\">[+]</span></td>\n");
+  echo("                      <td width=5%><span id=\"span_ferrAmbiente\" class=\"link\" onClick=\"MostraTr(this.id,".$i.",23);\">[+]</span></td>\n");
   echo("                    </tr>\n");
 
   foreach ($ordem_ferramentas as $cod=>$linha)
@@ -171,7 +171,14 @@
       echo("                    </tr>\n");
     }
   }
-  
+
+  echo("                    <tr class=\"altColor".($i++)%(2)."\" id=\"tr_ferrAmbiente_".($i)."\" style=\"display:none;\">\n");
+  echo("                      <td style=\"padding:10px 25px 10px 25px;\" align=left colspan=2>\n");
+  echo("                        <p style=\"font-weight:bold;\"> Usuarios Online</p>\n");	//TODO: Frase Hardcoded
+  echo("                        <p style=\"text-indent:15px;\"> Lista os alunos que estao online no curso.</p>\n");		//TODO: Frase Hardcoded
+  echo("                      </td>\n");
+  echo("                    </tr>\n");
+
   echo("                    <tr class=\"altColor".($i++)%(2)."\" id=\"tr_ferrAmbiente_".($i)."\" style=\"display:none;\">\n");
   echo("                      <td style=\"padding:10px 25px 10px 25px;\" align=left colspan=2>\n");
   echo("                        <p style=\"font-weight:bold;\">".RetornaFraseDaLista($lista_frases_ferramentas,47)."</p>\n");
