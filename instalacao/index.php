77cd91f86625a225d1f85315b611b760abb4a4f8
Lucas Lima <limma.lc@gmail.com>
2013-04-24 12:57:45 -0300

    Lancamento da nova instalacao

--- instalacao/index.php
+++ instalacao/index.php
@@ -3,7 +3,7 @@
 <!--
 -------------------------------------------------------------------------------
 
-    Arquivo : instalacao/index.php
+    Arquivo : index.php
 
     TelEduc - Ambiente de Ensino-Aprendizagem a Dist�ncia
     Copyright (C) 2001  NIED - Unicamp
@@ -37,64 +37,188 @@
 */
 
 /*==========================================================
-  ARQUIVO : instalacao/index.php
+  ARQUIVO : index.php
   ========================================================== */
 
-  include "instalacao.inc"; 
+include_once 'instalacao.inc';
 
-  ExibirCabecalho(0,"Instala��o");
+/* Versao do TelEduc */
+define("VERSAO", "4.3.1", true);
 
-  echo("
-    <script type=\"text/javascript\" language=javascript>
-      function Valida() {return true;}
-    </script>
-  ");
+/* Limpeza de variaveis usadas para controlar as mensagens exibidas
+ * na console e no "miolo" da pagina. */
+if (isset($console) && isset($content)) {
+	$console = "";
+	$content = "";
+}
 
-  AbreForm();
+/* Para simplificar, a instalacao possui um template, para
+ * o qual mandaremos o resultado da etapa atual. O controle de
+ * etapas sera feito atraves de uma variavel e posts para a mesma
+ * pagina. */
+if (isset($_GET['etapa'])) {
+	$etapa = $_GET['etapa'];
+}
+else if (isset($_POST['etapa'])) {
+	$etapa = $_POST['etapa'];
+}
+else {
+	$etapa = 0;
+}
 
-  switch(phpversion()){
-	  case('4.3.7'):
-		  $phpok = 0;
-		  break;
-	  case('4.3.8'):
-		  $phpok = 0;
-		  break;
-	  default:
-		  $phpok = 1;
-  }
+/* Salva os dados cadastrados nas etapas anteriores em variaveis de sessao. */
+if ($etapa > 0) {
+	session_start();
 
-  /* <!----------------- Tabelao -----------------> */
-  echo("<table cellpadding=\"0\" cellspacing=\"0\" class=\"tabExterna\">\n");
-  echo("<tr>\n");
-  /* <!----------------- Tabela Interna -----------------> */
-  echo("<td valign=\"top\"><table cellpadding=\"0\" cellspacing=\"0\" class=\"tabInterna\">\n");
-  echo("<tr><td style=\"padding-left: 150px; padding-top: 15px; padding-right: 150px; padding-bottom: 15px; font-size: small;\" align=\"left\">\n");
-
- if( ((bool) ini_get('register_globals')) && ($phpok))
-  {
-    Paragrafo("<font size=+2>Bem vindo � instala��o do ambiente TelEduc!</font>");
-    Paragrafo("");  //<br>
-    Paragrafo("<font color=\"#2a6686\">Leia atentamente as instru��es contidas em cada passo da instala��o</font>");
-    Paragrafo("Nas pr�ximas p�ginas, ser�o pedidas informa��es necess�rias para a instala��o do TelEduc.");
-    Paragrafo("Os campos j� preenchidos indicam quais seriam os valores necess�rios ao exemplo (conforme o arquivo <a href=# onClick='window.open(\"Guia_de_Instalacao.pdf\", \"Guia_de_Instalacao\", \"status=yes,toolbar=no,menubar=no,resizable=yes,scrollbars=yes\")'>Guia_de_Instalacao.pdf</a>).");
-    Paragrafo("Siga as instru��es para a correta instala��o do ambiente.");
-    echo("</td></tr></table>\n");
-    echo("</td></tr></table>\n");
-    EncerraPagina(0);
-  }
-  else  {
-	if(!(bool)ini_get('register_globals')){
-    		Paragrafo("Erro: Aparentemente a op��o register_globals do php est� desligada, a instala��o n�o pode continuar. Edite seu arquivo php.ini e altere a linha:");
-    		Paragrafo("<b> register_globals=Off</b> para <b> register_globals=On</b>");
-    		Paragrafo("Reinicie o servidor Apache e tente instalar o TelEduc novamente.");
+	if ($etapa == 2) {
+		$_SESSION['dbname'] = (isset($_POST['dbname']) ? $_POST['dbname'] : $_SESSION['dbname']);
+		$_SESSION['dbnamecurso'] = (isset($_POST['dbnamecurso']) ? $_POST['dbnamecurso'] : $_SESSION['dbnamecurso']);
+		$_SESSION['dbuser'] = (isset($_POST['dbuser']) ? $_POST['dbuser'] : $_SESSION['dbuser']);
+		$_SESSION['dbpwd'] = (isset($_POST['dbpwd']) ? $_POST['dbpwd'] : $_SESSION['dbpwd']);
+		$_SESSION['dbhost'] = (isset($_POST['dbhost']) ? $_POST['dbhost'] : $_SESSION['dbhost']);
+		$_SESSION['dbport'] = (isset($_POST['dbport']) ? $_POST['dbport'] : $_SESSION['dbport']);		
 	}
-	if(!$phpok){
-	    Paragrafo("Erro: Sua vers�o do PHP n�o � compat�vel com o sistema TelEduc, devido um bug na fun��o <b>glob()</b>.");
-	    Paragrafo("Para corrigir o problema instale o PHP <b>4.3.9 ou superior</b>.");
-	    Paragrafo("Ap�s atualizar o PHP, reinicie o servidor Apache e instale o TelEduc novamente.");
+	if ($etapa == 3) {
+		$_SESSION['host'] = (isset($_POST['host']) ? $_POST['host'] : $_SESSION['host']);
+		$_SESSION['www'] = (isset($_POST['www']) ? $_POST['www'] : $_SESSION['www']);
+		$_SESSION['arquivos'] = (isset($_POST['arquivos']) ? $_POST['arquivos'] : $_SESSION['arquivos']);
+		$_SESSION['sendmail'] = (isset($_POST['sendmail']) ? $_POST['sendmail'] : $_SESSION['sendmail']);
+	}
+	if ($etapa == 4) {
+		$_SESSION['admtele_nome'] = (isset($_POST['admtele_nome']) ? $_POST['admtele_nome'] : $_SESSION['admtele_nome']);
+		$_SESSION['admtele_email'] = (isset($_POST['admtele_email']) ? $_POST['admtele_email'] : $_SESSION['admtele_email']);
+		$_SESSION['admtele_senha'] = (isset($_POST['admtele_senha']) ? $_POST['admtele_senha'] : $_SESSION['admtele_senha']);
+	}
+}
+
+/* Verifica se os pre-requisitos da etapa estao devidamente instalados e
+ * monta o console com as mensagens de acordo com as etapas concluidas. */
+if ($erro =& verificaRequisitos($etapa, $erro) && !isset($_GET['bypass_anexo'])) {
+	exibeErro($etapa,$erro);
+}
+
+if ($etapa == 0) {
+	$content_header = "Bem-Vindo(a) à Instalação do TelEduc ".VERSAO."!";
+
+	$content .= "<p>Leia atentamente as instruções contidas em cada passo da instalação.</p>";
+	$content .= "<p>Em caso de dúvida, consulte o nosso <a href='doc/InstalacaoTelEduc4.pdf' target='_blank'>Guia de Instalação.</a></p>";
+
+	$content .= "<div class=formulario>";
+		$content .= "<form method='POST' action='index.php'>";
+			$content .= "<input type=hidden class='form' name=etapa value='1'/><br />";
+			$content .= "<input type=submit class='form' value='Instalar o TelEduc'/><br />";
+		$content .= "</form>";
+	$content .= "</div>";
+}
+else if ($etapa == 1) {
+	$console .= "<p class=feedbackp>O módulo php-mysql está instalado na versão correta. <img src='../cursos/aplic/imgs/certo.png' alt='com sucesso'></p>";
+	$console .= "<p class=feedbackp>A diretiva register_globals está habilitada. <img src='../cursos/aplic/imgs/certo.png' alt='com sucesso'></p>";
+	$console .= "<p class=feedbackp>A diretiva magic_quotes_gpc está habilitada. <img src='../cursos/aplic/imgs/certo.png' alt='com sucesso'></p>";
+
+	$content_header = "Banco de Dados e Arquivo de Configuração <span class=etapa>Etapa <font color='#FF0000'>1</font> de 4</span>";
+
+	$content .= "<p>O TelEduc utiliza os seguintes bancos de dados:</p>";
+	$content .= "<p style='margin-top:10px'>&nbsp;&nbsp;<b>Banco Geral</b> - Contém configurações e informações do ambiente.</p>";
+	$content .= "<p>&nbsp;&nbsp;<b>Banco do Curso X</b> - Contém as informações do curso com o código <b>X</b>.</p>";
+	$content .= "<br />";
+
+	$content .= "<p>As informações para acesso aos bancos de dados serão gravadas a seguir em:</p>";
+	$content .= "<pre>teleduc4/cursos/aplic/bibliotecas/teleduc.inc</pre>";
+	$content .= "<br />";
+
+	$content .= "<div class=formulario>";
+		$content .= "<form id='formInstall' name='formInstall' method='POST' action='index.php' onSubmit='return valida_form(document.formInstall, 1);'>";
+			$content .= "<label class='form' for=dbname>Banco Geral:</label>";
+			$content .= "<input class='form' size=25 type=text name=dbname value='".(isset($_SESSION['dbname']) ? $_SESSION['dbname'] : 'TelEduc4')."'/><br />";
+			$content .= "<label class='form' for=dbnamecurso>Prefixo do Banco do Curso:</label>";
+			$content .= "<input class='form' size=25 type=text name=dbnamecurso value='".(isset($_SESSION['dbnamecurso']) ? $_SESSION['dbnamecurso'] : 'TelEduc4Curso_')."'/><br />";
+			$content .= "<label class='form' for=dbuser>Usuario do MySQL:</label>";
+			$content .= "<input class='form' size=25 type=text name=dbuser value='".(isset($_SESSION['dbuser']) ? $_SESSION['dbuser'] : 'usuario')."'/><br />";
+			$content .= "<label class='form' for=dbpwd>Senha do MySQL:</label>";
+			$content .= "<input class='form' size=25 type=password name=dbpwd value='".(isset($_SESSION['dbpwd']) ? $_SESSION['dbpwd'] : 'senha')."'/><br />";
+			$content .= "<label class='form' for=dbhost>Servidor do MySQL:</label>";
+			$content .= "<input class='form' size=25 type=text name=dbhost value='".(isset($_SESSION['dbhost']) ? $_SESSION['dbhost'] : 'localhost')."'/><br />";
+			$content .= "<label class='form' for=dbport>Porta do MySQL:</label>";
+			$content .= "<input class='form' size=25 type=text name=dbport value='".(isset($_SESSION['dbport']) ? $_SESSION['dbport'] : '3306')."'/><br />";
+			$content .= "<input class='form' type=hidden name=etapa value='2'/><br />";
+			$content .= "<input class='form' type=submit value='Continuar Instalação' /><br />";
+		$content .= "</form>";
+	$content .= "</div>";
+}
+else if ($etapa == 2) {	
+	$console .= "<p class=feedbackp>O banco de dados foi criado com sucesso. <img src='../cursos/aplic/imgs/certo.png' alt='com sucesso'></p>";
+	$console .= "<p class=feedbackp>O banco de dados foi inicializado com sucesso. <img src='../cursos/aplic/imgs/certo.png' alt='com sucesso'></p>";
+	$console .= "<p class=feedbackp>As bases de dados foram importadas com sucesso. <img src='../cursos/aplic/imgs/certo.png' alt='com sucesso'></p>";
+	$console .= "<p class=feedbackp>O arquivo de configuração foi criado com sucesso. <img src='../cursos/aplic/imgs/certo.png' alt='com sucesso'></p>";
 	
-	}
-    echo("</td></tr></table>\n");
-    echo("</td></tr></table>\n");
-  }
+	$content_header = "Servidor e Diretórios <span class=etapa>Etapa <font color='#FF0000'>2</font> de 4</span>";
+
+	$content .= "<p>Nesta etapa será necessário informar o nome do servidor e o caminho do TelEduc, que podem ser verificados dentro do navegador utilizando sua barra de endereços:</p>";
+	$content .= "<pre>http://nome.do.servidor/caminho/do/teleduc/instalacao</pre>";
+	$content .= "<br/>";
+	$content .= "<p>É necessário uma pasta para armazenar os arquivos dos usuários, certifique-se de que o servidor web tem as permissões necessárias para escrever nessa pasta.</p>";
+	$content .= "<br />";
+	$content .= "<p>O caminho para o executável do sendmail é necessário para o envio de emails.</p>";
+	$content .= "<br />";
+
+	$content .= "<div class=formulario>";
+		$content .= "<form id='formInstall' name='formInstall' method='POST' action='index.php' onSubmit='return valida_form(document.formInstall, 2);'>";
+			$content .= "<label class='form' for=host>Nome do Servidor</label>";
+			$content .= "<input type=text size=25 class='form' name=host value='".$_SERVER['SERVER_NAME']."'/><br />";
+			$content .= "<label class='form' for=www>Caminho do TelEduc</label>";
+			$content .= "<input type=text size=25 class='form' name=www value='".str_replace("instalacao/index.php", "", $_SERVER['PHP_SELF'])."'/><br />";
+			$content .= "<label class='form' for=arquivos>Arquivos de Usuário</label>";
+			$content .= "<input type=text size=25 class='form' name=arquivos value='".str_replace("public_html/teleduc4/instalacao", "arquivos", getcwd())."'/><br />";
+			$content .= "<label class='form' for=sendmail>Caminho do Sendmail</label>";
+			$content .= "<input type=text size=25 class='form' name=sendmail value='/usr/sbin/sendmail'/><br />";
+			$content .= "<input type=hidden name=etapa value='3'/><br />";
+			$content .= "<input class='form' type=submit value='Continuar Instalação' /><br />";
+		$content .= "</form>";
+	$content .= "</div>";
+}
+else if ($etapa == 3) {
+	$console .= "<p class=feedbackp>O diretório de arquivos foi configurado com sucesso. <img src='../cursos/aplic/imgs/certo.png' alt='com sucesso'></p>";
+
+	$content_header = "Administrador do Ambiente <span class=etapa>Etapa <font color='#FF0000'>3</font> de 4</span>";
+
+	$content .= "<p>O TelEduc utiliza o usuário <b>admtele</b> para acesso à administração do ambiente.</p>";
+	$content .= "<br />";
+	$content .= "<p>Preencha os campos abaixo com os dados do administrador do ambiente.</p>";
+	$content .= "<br />";
+
+	$content .= "<div class=formulario>";
+		$content .= "<form id='formInstall' name='formInstall' method='POST' action='index.php' onSubmit='return valida_form(document.formInstall, 3);'>";
+			$content .= "<label class='form' for=admtele_nome>Nome</label>";
+			$content .= "<input type=text size=25 class='form' name=admtele_nome value='".(isset($_SESSION['admtele_nome']) ? $_SESSION['admtele_nome'] : 'Nome Sobrenome')."'/><br />";
+			$content .= "<label class='form' for=admtele_email>E-Mail</label>";
+			$content .= "<input type=text size=25 class='form' name=admtele_email value='".(isset($_SESSION['admtele_email']) ? $_SESSION['admtele_email'] : 'nome@email.com')."'/><br />";
+			$content .= "<label class='form' for=admtele_senha>Senha</label>";
+			$content .= "<input type=password size=25 class='form' name=admtele_senha value='".(isset($_SESSION['admtele_senha']) ? $_SESSION['admtele_senha'] : 'admtele')."'/><br />";
+			$content .= "<input type=hidden name=etapa value='4'/><br />";
+			$content .= "<input type=submit value='Prosseguir' class='form'/><br />";
+		$content .= "</form>";
+	$content .= "</div>"; 
+}
+else if ($etapa == 4) {
+	$console .= "<p class=feedbackp>Os dados do administrador do ambiente foram salvos com sucesso. <img src='../cursos/aplic/imgs/certo.png' alt='com sucesso'></p>";
+	
+	$content_header = "Fim da Instalação <span class=etapa>Etapa <font color='#FF0000'>4</font> de 4</span>";
+	
+	$content .= "<b><p style='text-align: center; font-size: 20px;'>O TelEduc foi instalado e está pronto para uso!</p></b>";
+	$content .= "<br />";
+	$content .= "<p>Recomendamos a remoção da pasta de instalação por questões de segurança.</p>";
+	$content .= "<br/>";
+	$content .= "<p>Habilite a notificação de novidades via email adicionando ao crontab:</p>";
+	$content .= "<pre>0 17 * * * /usr/bin/lynx -dump http://hera.nied.unicamp.br/~teleduc4/scripts/notificar.php?notificar_email=1";
+	$content .= "<br/>";
+	$content .= "0 9 * * * /usr/bin/lynx -dump http://hera.nied.unicamp.br/~teleduc4/scripts/notificar.php?notificar_email=2";
+	$content .= "<br/>";
+	$content .= "0 18 * * * /usr/bin/lynx -dump http://hera.nied.unicamp.br/~teleduc4/scripts/notificar.php?notificar_email=2</pre>";
+	$content .= "<br />";
+	
+	$content .= "<input type=submit value='Entrar' onClick=\"document.location='../'\" class='form'/><br />";
+}
+
+include 'template_instalacao.php';
+exit();
 ?>
