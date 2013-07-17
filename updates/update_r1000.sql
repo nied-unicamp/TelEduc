b5d84eb6a7f08fb5d333d7d66a8645b2fc908619
lucaslm <lucaslmadureira@gmail.com>
2013-07-17 17:13:56 -0300

    Arrumando/adicionando alguns textos

---
+++ updates/update_r1000.sql
@@ -0,0 +1,17 @@
+/*Base Geral*/
+
+UPDATE `Lingua_textos` set texto = 'Colaboradores'                        where cod_texto = 55 and cod_lingua = 1 and cod_ferramenta = 18;
+UPDATE `Lingua_textos` set texto = '(N&atilde;o h&aacute; colaboradores)' where cod_texto = 63 and cod_lingua = 1 and cod_ferramenta = 18;
+UPDATE `Lingua_textos` set texto = 'Colaborador'                          where cod_texto = 67 and cod_lingua = 1 and cod_ferramenta = 18;
+
+UPDATE `Lingua_textos` set texto = 'Colaboradores'          where cod_texto = 55 and cod_lingua = 2 and cod_ferramenta = 18;
+UPDATE `Lingua_textos` set texto = '(No hay colaboradores)' where cod_texto = 63 and cod_lingua = 2 and cod_ferramenta = 18;
+UPDATE `Lingua_textos` set texto = 'Colaborador'            where cod_texto = 67 and cod_lingua = 2 and cod_ferramenta = 18;
+
+UPDATE `Lingua_textos` set texto = 'Collaborators'                where cod_texto = 55 and cod_lingua = 3 and cod_ferramenta = 18;
+UPDATE `Lingua_textos` set texto = '(There are no collaborators)' where cod_texto = 63 and cod_lingua = 3 and cod_ferramenta = 18;
+UPDATE `Lingua_textos` set texto = 'Collaborator'                 where cod_texto = 67 and cod_lingua = 3 and cod_ferramenta = 18;
+
+UPDATE `Lingua_textos` set texto = 'Colaboradores(as)'                        where cod_texto = 55 and cod_lingua = 4 and cod_ferramenta = 18;
+UPDATE `Lingua_textos` set texto = '(N&atilde;o h&aacute; colaboradores(as))' where cod_texto = 63 and cod_lingua = 4 and cod_ferramenta = 18;
+UPDATE `Lingua_textos` set texto = 'Colaborador(a)'                           where cod_texto = 67 and cod_lingua = 4 and cod_ferramenta = 18;
