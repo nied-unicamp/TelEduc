db583d751d2bffd3c6d2623a49446845b30cba08
lucaslm <lucaslmadureira@gmail.com>
2013-10-25 17:28:40 -0200

    Adicionando arquivo com updates no BD

--- updates/update_4.3.1_4.sql
+++ updates/update_4.3.1_4.sql
@@ -1,17 +1,13 @@
 /* Base Geral*/
 
 INSERT IGNORE INTO `Lingua_textos` (`cod_texto`, `cod_lingua`, `cod_ferramenta`, `texto`) VALUES
-(115, 1, 10, 'Erro ao marcar a sessão.'),
-(115, 3, 10, 'Error while scheduling the session.'),
-(104, 4, 10, 'Valor'),
-(105, 4, 10, 'Objetivos'),
-(106, 4, 10, 'Critérios'),
-(107, 4, 10, 'Incluir Avaliação'),
-(108, 4, 10, 'Alterar Avaliação'),
-(109, 4, 10, 'Apagar Avaliação'),
-(110, 4, 10, 'N&atilde;o definidos'),
-(111, 4, 10, 'O campo Valor não pode ser vazio'),
-(112, 4, 10, 'Digitaste caracteres estranhos no campo Valor.'),
-(113, 4, 10, 'Usa apenas dígitos de 0 a 9 e o ponto ( . ) ou a vírgula ( , ) para este campo (exemplo: 7.5).'),
-(114, 4, 10, 'A avaliacão não pode ter valor negativo.'),
-(115, 4, 10, 'Erro ao marcar a sessão.');
+(110, 1, 10, 'N&atilde;o definidos'),
+(111, 1, 10, 'O campo Valor não pode ser vazio'),
+(112, 1, 10, 'Você digitou caracteres estranhos no campo Valor.'),
+(113, 1, 10, 'Use apenas dí­gitos de 0 a 9 e o ponto ( . ) ou a ví­rgula ( , ) para este campo (exemplo: 7.5).'),
+(114, 1, 10, 'A avaliação não pode ter valor negativo.'),
+(110, 3, 10, 'Not defined'),
+(111, 3, 10, 'The field Value cannot be empty'),
+(112, 3, 10, 'You typed strange characters on the field Value'),
+(113, 3, 10, 'Type just numbers from 0 to 9 and the dot ( . ) or the comma ( , ) for this field (example: 7.5).'),
+(114, 3, 10, 'The evaluation cannot contain negative values.');
