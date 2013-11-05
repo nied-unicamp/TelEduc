c07377869d91979525db7e359bf7255344314fc6
Camila Abreu <camila.kna@hotmail.com>
2013-11-05 16:19:23 -0500

    Adicionando arquivo de update do banco de dados

---
+++ updates/update_4.3.1_6.sql
@@ -0,0 +1,9 @@
+/* Base Geral*/
+
+INSERT IGNORE INTO `Lingua_textos` (`cod_texto`, `cod_lingua`, `cod_ferramenta`, `texto`) VALUES
+(234, 1, 23, 'Arquivo(s) desocultado(s) com sucesso.'),
+(235, 1, 23, '&Uacute;ltima modifica&ccedil;&atilde;o em'),
+(236, 1, 23, 'Justificar'),
+(234, 3, 23, 'File(s) unhidden succesfully.'),
+(235, 3, 23, 'Last modified on'),
+(236, 3, 23, 'Justify');
