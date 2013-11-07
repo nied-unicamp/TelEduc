7d0a43696ae70b4832a471e3992e409774575781
lucaslm <lucaslmadureira@gmail.com>
2013-11-07 16:00:43 -0200

    Adicionando possibilidade de ferramentas passarem arquivos js e css para o cabeçalho da página

--- cursos/aplic/topo_tela.php
+++ cursos/aplic/topo_tela.php
@@ -60,15 +60,18 @@
 
   // Se diferente, entï¿½o lï¿½ngua do curso ï¿½ diferente da lï¿½ngua do usuï¿½rio, atualiza a lista de frases
 //  if($auxiliar != $_SESSION['cod_lingua_s']){
-//  	unset($_SESSION['lista_frases_s']);
+//    unset($_SESSION['lista_frases_s']);
 //  }
-  
-  
+
+  if (!isset($cod_ferramenta))
+    $cod_ferramenta = 1; /* Agenda */
+
   $lista_frases_menu=RetornaListaDeFrases($sock,-4);
 
   $lista_frases=RetornaListaDeFrases($sock,$cod_ferramenta);
 
   $lista_frases_geral=RetornaListaDeFrases($sock,-1);
+
   $tela_ordem_ferramentas=RetornaOrdemFerramentas($sock);
   $tela_lista_ferramentas=RetornaListaFerramentas($sock);
   $tela_lista_titulos=RetornaListaTitulos($sock, $_SESSION['cod_lingua_s']);
@@ -84,28 +87,21 @@
   $cod_usuario = RetornaCodigoUsuarioCurso($sock, $cod_usuario_global, $cod_curso);
   VerificaAcessoAoCurso($sock,$cod_curso,$cod_usuario);
 
-  $tela_visitante     = EVisitante($sock,$cod_curso,$cod_usuario);
+  $tela_formador      = EFormador($sock, $cod_curso, $cod_usuario);
+  $tela_formadormesmo = EFormadorMesmo($sock, $cod_curso, $cod_usuario);
+  // booleano, indica se usuario eh colaborador
+  $tela_colaborador   = EColaborador($sock, $cod_curso, $cod_usuario);
+  // booleano, indica se usuario eh visitante
+  $tela_visitante     = EVisitante($sock, $cod_curso, $cod_usuario);
 
-  $tela_formador          = EFormador($sock,$cod_curso,$cod_usuario);
-  $tela_formadormesmo     = EFormadorMesmo($sock,$cod_curso,$cod_usuario);
-
-  // booleano, indica se usuario eh convidado
-  $tela_convidado         = EConvidado ($sock, $cod_usuario, $cod_curso);
-  // especifica que tipo de convidado eh
-  $tela_convidado_ativo   = EConvidadoAtivo($sock, $cod_usuario, $cod_curso);
-  $tela_convidado_passivo = EConvidadoPassivo($sock, $cod_usuario, $cod_curso);
-  
   $SalvarEmArquivo = (!isset($SalvarEmArquivo) || $SalvarEmArquivo != 1) ? 0 : 1;
-  
+
   Desconectar($sock);
 
   $sock=Conectar($cod_curso);
 
-  VerificaAcessoAFerramenta($sock,$cod_curso,$cod_usuario,0);
-  MarcaAcesso($sock,$cod_usuario,$cod_ferramenta);
-
-  if (!isset($cod_ferramenta))
-    $cod_ferramenta=1; /* Agenda */
+  VerificaAcessoAFerramenta($sock, $cod_curso, $cod_usuario, $cod_ferramenta);
+  MarcaAcesso($sock, $cod_usuario, $cod_ferramenta);
 
   echo("<!DOCTYPE HTML SYSTEM \"http://teleduc.nied.unicamp.br/~teleduc/loose-custom.dtd\">\n");
   echo("<html lang=\"pt\">\n"); 
@@ -118,16 +114,34 @@
   echo("    <meta name=\"copyright\" content=\"TelEduc\">\n");
   echo("    <meta http-equiv=\"Content-Type\" content=\"text/html; charset=iso-8859-1\">\n");
   echo("    <link rel=\"shortcut icon\" href=\"../../../favicon.ico\" />\n");
-  
-  $estilos_css = array("../js-css/ambiente.css",
-                       "../js-css/navegacao.css",
-                       "../js-css/tabelas.css",
-                       "../js-css/dhtmlgoodies_calendar.css");
 
-  $codigos_js = array("../bibliotecas/dhtmllib.js",
-                      "../js-css/dhtmlgoodies_calendar.js",
-                      "../js-css/jscript.js",
-                      "../js-css/chat.js");
+  $estilos_css_default = array("../js-css/ambiente.css",
+                               "../js-css/navegacao.css",
+                               "../js-css/tabelas.css",
+                               "../js-css/dhtmlgoodies_calendar.css");
+
+  $codigos_js_default = array("../bibliotecas/dhtmllib.js",
+                              "../js-css/dhtmlgoodies_calendar.js",
+                              "../js-css/jscript.js",
+                              //"../js-css/chat.js"
+                        );
+
+  // Se antes da inclusão de topo_tela.php, a página já indicou 
+  // arquivos css ou js, devemos incluí-los no cabeçalho da página.
+
+  if (isset($estilos_css) && is_array($estilos_css)) {
+    $estilos_css = array_merge($estilos_css_default, $estilos_css);
+  }
+  else {
+    $estilos_css = $estilos_css_default;
+  }
+
+  if (isset($codigos_js) && is_array($codigos_js)) {
+    $codigos_js = array_merge($codigos_js_default, $codigos_js);
+  }
+  else {
+    $codigos_js = $codigos_js_default;
+  }
 
   /* Se estamos salvando a pagina em um arquivo, manter o css inline e sem javascript.
    * Caso contrario podemos servi-los normalmente sob a forma de links.
