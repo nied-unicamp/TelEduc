19504ea3233673ccc6c4553fe0084b5aae5cfdb3
lucaslm <lucaslmadureira@gmail.com>
2013-11-12 16:46:45 -0200

    Removendo papel convidado da ferramenta Diário

--- cursos/aplic/diario/diario.php
+++ cursos/aplic/diario/diario.php
@@ -278,6 +278,8 @@
 
   echo("        <td width=\"100%\" valign=\"top\" id=\"conteudo\">\n");
 
+  ExpulsaVisitante($sock, $cod_curso, $cod_usuario);
+
   /* 1 - Diario de Bordo */
   echo("          <h4>".RetornaFraseDaLista($lista_frases, 1)."</h4>\n");
 
