ec7b28a853f9acdaaf37769d0d56a37334ad95d3
lucaslm <lucaslmadureira@gmail.com>
2014-01-27 16:27:25 -0200

    Removendo debug comitado por acidente.

--- cursos/aplic/perfil/enviar_foto2.php
+++ cursos/aplic/perfil/enviar_foto2.php
@@ -68,7 +68,6 @@
 
   $ext=$pieces[count($pieces)-1];
   $nomefinal="cod_usuario_".$cod_usuario.".".$ext;
-  error_log("\$nomefinal = ".$nomefinal);
   if(!AtualizaArquivo($sock,$arquivo,$nomefinal,$diretorio_temp,$cod_curso)){
 
    if (isset($alunocod))
