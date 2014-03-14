a64a6c766e523c05b0c4f92b9f6ce15d54bfbd70
lucaslm <lucaslmadureira@gmail.com>
2014-03-14 17:09:55 -0300

    Repondo valores da extração que foram perdidos nas versões mais recentes da instalação. References #472 (continuação)

---
+++ updates/update_4.3.1_14.sql
@@ -0,0 +1,5 @@
+/* Base Geral*/
+
+INSERT IGNORE INTO Extracao VALUES ('diretorio', '{Cursos_extraidos.codigo}');
+INSERT IGNORE INTO Extracao VALUES ('select_query','select codigo from Cursos_extraidos where codigo = \']codigo[\'');
+INSERT IGNORE INTO Extracao VALUES ('compactar','S');
