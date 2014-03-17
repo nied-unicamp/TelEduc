7c04a4104af421247bebe207b04d87dbd1bdc2f2
lucaslm <lucaslmadureira@gmail.com>
2014-03-17 14:21:48 -0300

    Pequena alteração para evitar que se insira tuplas duplicadas na tabela de Extracao

--- updates/update_4.3.1_14.sql
+++ updates/update_4.3.1_14.sql
@@ -1,5 +1,8 @@
 /* Base Geral*/
 
+ALTER TABLE Diretorio add PRIMARY KEY(item);
+ALTER TABLE Extracao  add PRIMARY KEY(item);
+
 INSERT IGNORE INTO Extracao VALUES ('diretorio', '{Cursos_extraidos.codigo}');
 INSERT IGNORE INTO Extracao VALUES ('select_query','select codigo from Cursos_extraidos where codigo = \']codigo[\'');
 INSERT IGNORE INTO Extracao VALUES ('compactar','S');
