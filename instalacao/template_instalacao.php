5e12f40d6e0b57cdffce6f8f3cede46fd20aedc9
Camila Abreu <camila.kna@hotmail.com>
2015-03-24 16:05:59 -0300

    Alterações na instalação

--- instalacao/template_instalacao.php
+++ instalacao/template_instalacao.php
@@ -105,7 +105,7 @@
 
 <body>
 	<div class="container">
-		<div id="logo"><img src="../imgs/logo.gif"/></div>
+		<div id="logo"><img src="../web-content/imgs/logo.gif"/></div>
 		<div id="header"><h1>Instala&ccedil;&atilde;o do TelEduc <?php echo VERSAO; ?></h1></div>
 		<div class="content-header">
 			<p><?php echo $content_header;?></p>
@@ -119,9 +119,9 @@
 		</div>
 		<div class="footer">
 			<div id="logo_sampler">
-				<a href="http://www.nied.unicamp.br"><img src="../imgs/logoNied.gif" alt="nied" border="0" style="margin-right:8px;" /></a>
-				<a href="http://www.ic.unicamp.br"><img src="../imgs/logoInstComp.gif" alt="Instituto de Computa&ccedil;&atilde;o" border="0" style="margin-right:6px;" /></a>
-				<a href="http://www.unicamp.br"><img src="../imgs/logoUnicamp.gif" alt="UNICAMP" border="0" /></a>
+				<a href="http://www.nied.unicamp.br"><img src="../web-content/imgs/logoNied.gif" alt="nied" border="0" style="margin-right:8px;" /></a>
+				<a href="http://www.ic.unicamp.br"><img src="../web-content/imgs/logoInstComp.gif" alt="Instituto de Computa&ccedil;&atilde;o" border="0" style="margin-right:6px;" /></a>
+				<a href="http://www.unicamp.br"><img src="../web-content/imgs/logoUnicamp.gif" alt="UNICAMP" border="0" /></a>
 			</div>
 			<div id="signature">
 				<p>2013 - TelEduc - Todos os direitos reservados. All rights reserved - NIED - UNICAMP</p>
